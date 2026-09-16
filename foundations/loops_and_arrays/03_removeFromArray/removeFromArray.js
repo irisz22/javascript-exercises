9cf4926ffd506c7fe79bcea6c4d3d0a525096097
Iris <75283241+irisz22@users.noreply.github.com>
2026-09-16 19:04:03 -0400

    completed exercise 3 loops & arrays

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) { // args w/ rest operator
+
+    return arr.filter((cur) => !args.includes(cur));
+    // if args includes cur, then it filters out cur!
 };
 
 // Do not edit below this line

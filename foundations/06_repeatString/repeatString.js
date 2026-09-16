057b83451110649bcf23b5821f82255217d51953
Iris <75283241+irisz22@users.noreply.github.com>
2026-09-16 10:01:16 -0400

    completed exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = ((string, num) => {
+    if (num < 0) {
+        return 'ERROR';
+    }
 
-};
+    let newString = '';
+
+    while (num > 0) {
+        newString += string;
+        num--;
+    }
+
+    return newString;
+});
 
 // Do not edit below this line
 module.exports = repeatString;

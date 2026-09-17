d540d8aef8d0154f128debee8af78e779505a61e
Iris <75283241+irisz22@users.noreply.github.com>
2026-09-17 06:42:56 -0400

    completed exercise 4

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // an option is to set min and max as parameter names
+    //and choose it from the very beginning!
+    
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
+
+    let arr = [];
+    let smaller = Math.min (num1, num2);
+    let larger = Math.max(num1, num2);
+
+    num1 = smaller;
+    num2 = larger;
+
+    for (let i = num1; i <= num2; i++) {
+        arr.push(i);
+    }
+
+    return arr.reduce(((cur, total) => cur + total), 0);
 
 };
 

2c2330cae6282ff9076bbef8cf5b5920cbe45628
Iris <75283241+irisz22@users.noreply.github.com>
2026-09-23 17:45:51 -0400

    completed fibonacci exercise

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(val) {
+    const num = Number(val);
+
+    if (num < 0 || isNaN(num) || !Number.isInteger(num)) {
+        return "OOPS";
+    }
+
+    if (num === 0) return 0;
+    if (num === 1 || num === 2) return 1;
+
+    let prev1 = 1; // second ever value
+    let prev2 = 1; // first ever value
+    let cur = 0;
+
+    for (let i = 3; i <= num; i++) {
+        cur = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = cur;
+    }
+
+    return cur;
 
 };
 

9b1656f320606d34f59fadc0bd1f82ce30fa1696
Iris <75283241+irisz22@users.noreply.github.com>
2026-09-23 16:44:49 -0400

    completed palindromes exercise

--- foundations/object_basics/02_palindromes/palindromes.js
+++ foundations/object_basics/02_palindromes/palindromes.js
@@ -1,4 +1,23 @@
-const palindromes = function () {
+const palindromes = function (myStr) {
+    myStr = myStr.toLowerCase();
+    const filterStr = (str) => str.replace(/[^a-zA-Z0-9]/g, "");
+
+    let newStr = filterStr(myStr);
+    let strArr = [...newStr];
+
+    let start = 0;
+    let end = strArr.length - 1;
+
+    while(strArr[start] === strArr[end]) {
+        start++;
+        end--;
+        if (start == end) {
+            return true;
+        } else if (start == end - 1) {
+            return true;
+        }
+    }
+    return false;
 
 };
 
